aaa85ca5dff9a67aaa222ae0d3e306d163792b6c
bukatovavala-hub <bukatovavala@gmail.com>
2026-01-15 16:41:19 +0500

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -8,4 +8,5 @@ def post_new_order(body):
 def get_order_by_track(track_id):
     return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_PATH + str(track_id))
 
-	assert assertion_code_200() == data.status_code_200
+	
+
